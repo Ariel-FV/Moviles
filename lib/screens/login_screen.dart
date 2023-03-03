e8bedbce2e4f167c720841945f49926175521ef9
Ariel-FV <18031781@itcelaya.edu.mx>
2023-03-03 14:02:44 -0600

    v2

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pmsna1/responsive.dart';
 import 'package:social_login_buttons/social_login_buttons.dart';
 import '../widgets/loading_modal_widget.dart';
 
@@ -27,15 +28,12 @@ final txtPass = TextFormField(
 );
 final spaceHorizontal = SizedBox(height: 10,);
 
-
-
 final btnGoggle = SocialLoginButton(buttonType: SocialLoginButtonType.google, onPressed:(){}, );
 final btnFacebook = SocialLoginButton(buttonType: SocialLoginButtonType.facebook, onPressed: (){},);
 final btnGitHub = SocialLoginButton(buttonType: SocialLoginButtonType.github, onPressed: (){});
 
-
-
-final imglogo = Image.asset('assets/logo.png', height: 200,);
+final imglogo = Image.asset('assets/logo.png', height: 350, width: 350);
+final imglogoD = Image.asset('assets/logo.png', height: 300, width: 300,);
 
   @override
   Widget build(BuildContext context) {
@@ -58,54 +56,177 @@ final imglogo = Image.asset('assets/logo.png', height: 200,);
           setState(() {});
           Navigator.pushNamed(context, '/dash');
         });
-      },
+      }, 
     );
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                opacity: .6, 
-                fit: BoxFit.cover, 
-                image: AssetImage('assets/fondo.jpg')
-              )
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Stack(
-                alignment: Alignment.topCenter,
-                children: [
-                  Column(
-                    
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                        
-                        txtEmail,
-                        spaceHorizontal,
-                        txtPass,
-                        spaceHorizontal,
-                        btnEmail,
-                        spaceHorizontal,
-                        btnFacebook,
-                        spaceHorizontal,
-                        btnGoggle,
-                        spaceHorizontal,
-                        btnGitHub,
-                        spaceHorizontal,
-                        txtRegister
-                    ],
-                  ),
-                  Positioned(top:100, child: imglogo)
-                ],
+    return Responsive(
+      mobile:  Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  opacity: .6, 
+                  fit: BoxFit.cover, 
+                  image: AssetImage('assets/fondotf.jpg')
+                )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Column(
+                      
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                          imglogo,
+                          txtEmail,
+                          spaceHorizontal,
+                          txtPass,
+                          spaceHorizontal,
+                          btnEmail,
+                          spaceHorizontal,
+                          btnFacebook,
+                          spaceHorizontal,
+                          btnGoggle,
+                          spaceHorizontal,
+                          btnGitHub,
+                   
+                          txtRegister
+                      ],
+                    ),
+                    //Positioned(top:100, child: imglogo)
+                  ],
+                ),
               ),
             ),
-          ),
-          isLoading ? const LoadingModalWidget() : Container()
-       ],
+            isLoading ? const LoadingModalWidget() : Container()
+         ],
+        ),
       ),
+      tablet: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  opacity: .6, 
+                  fit: BoxFit.cover, 
+                  image: AssetImage('assets/fondotb.jpg')
+                )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                      Flexible(child:   
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            imglogoD,
+                          ],
+                        )
+                      ),
+
+                      Flexible(child: 
+                        Column(
+                          
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                              txtEmail,
+                             spaceHorizontal,
+                              txtPass,
+                              spaceHorizontal,
+                              btnEmail,
+                              spaceHorizontal,
+                              btnFacebook,
+                              spaceHorizontal,
+                              btnGoggle,
+                              spaceHorizontal,
+                              btnGitHub,
+                              spaceHorizontal,
+                              txtRegister
+                          ],
+                        ),
+                      ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            isLoading ? const LoadingModalWidget() : Container()
+         ],
+        ),
+      ),
+      desktop: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  opacity: .6, 
+                  fit: BoxFit.cover, 
+                  image: AssetImage('assets/fondopc.png')
+                )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                      Flexible(child:   
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            imglogoD,
+                          ],
+                        )
+                      ),
+
+                      Flexible(child: 
+                        Column(
+                          
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                              txtEmail,
+                             spaceHorizontal,
+                              txtPass,
+                              spaceHorizontal,
+                              btnEmail,
+                              spaceHorizontal,
+                              btnFacebook,
+                              spaceHorizontal,
+                              btnGoggle,
+                              spaceHorizontal,
+                              btnGitHub,
+                              spaceHorizontal,
+                              txtRegister
+                          ],
+                        ),
+                      ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            isLoading ? const LoadingModalWidget() : Container()
+         ],
+        ),
+      ),
+      
     );
   }
 }
